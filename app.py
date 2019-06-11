d10676e1abc4460b04b656033bc92dca822834ac
Paris Nakita Kejser <contact@parisnakitakejser.com>
2019-06-11 17:56:38 +0200

    (update) support dummy messagest to sending back to client when its connect

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very-seceret'
 socketio = SocketIO(app)
 
 
+messeges = [{
+    'color': '#abc',
+    'user': 'User 1',
+    'msg': 'Hello world, welcom to CocketChat'
+}, {
+    'color': '#cba',
+    'user': 'User 2',
+    'msg': 'Thanks for invete me!'
+}, {
+    'color': '#bac',
+    'user': 'User 3',
+    'msg': 'Somone want to chat?'
+}]
+
 # Flask or Socket.IO transport layer can be added here!
 
 @socketio.on('connect')
 def client_connect():
     print('client connected')
+    emit('MESSAGE', messeges)
 
 @socketio.on('disconnect')
 def client_disconnect():
