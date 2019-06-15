6f1cf955aa4ee267d53354f21fad4c67bb8bd238
Paris Nakita Kejser <contact@parisnakitakejser.com>
2019-06-15 11:56:16 +0200

    (update) support request.sid and user connect/disconnect handler

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO, emit
+
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very-seceret'
@@ -9,7 +11,7 @@ socketio = SocketIO(app)
 messeges = [{
     'color': '#abc',
     'user': 'User 1',
-    'msg': 'Hello world, welcom to CocketChat'
+    'msg': 'Hello world, welcom to SocketChat'
 }, {
     'color': '#cba',
     'user': 'User 2',
@@ -22,14 +24,52 @@ messeges = [{
 
 # Flask or Socket.IO transport layer can be added here!
 
+users_online = {}
+guest_vistor_count = 0
+
 @socketio.on('connect')
 def client_connect():
+    global guest_vistor_count
+
     print('client connected')
+
+    users_online[request.sid] = {
+        'username': f'Guest #{guest_vistor_count+1}',
+        'rank': 'guest',
+        'connected_time': time.time()
+    }
+    guest_vistor_count += 1
+
+    print(f'online users count: {len(users_online)}')
+    print(users_online)
     emit('MESSAGE', messeges)
+    emit('USER_DATA', users_online[request.sid])
+
 
 @socketio.on('disconnect')
 def client_disconnect():
     print('client disconnect')
+    del users_online[request.sid]
+
+
+@socketio.on('SEND_MESSAGE')
+def client_send_message(data):
+    if data['msg'].strip() != '':
+        print('client send message')
+
+        if len(messeges) >= 5:
+            print('pop the old messagt from the messagt queue')
+            messeges.pop(0)
+
+        messeges.append({
+            'color': data['color'],
+            'user': users_online[request.sid]['username'],
+            'msg': data['msg']
+        })
+
+        emit('MESSAGE', messeges, broadcast=True)
+    else:
+        print('empty message, its not sending back')
 
 
 if __name__ == '__main__':
