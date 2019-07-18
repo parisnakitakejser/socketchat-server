2e7906acccd1faf4eaa7237b0f876973391666a7
Paris Nakita Kejser <contact@parisnakitakejser.com>
2019-07-18 20:37:16 +0200

    (update) add support for join and leave room and the user can now change chat room

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 import time
 
@@ -8,19 +8,10 @@ app.config['SECRET_KEY'] = 'very-seceret'
 socketio = SocketIO(app)
 
 
-messeges = [{
-    'color': '#abc',
-    'user': 'User 1',
-    'msg': 'Hello world, welcom to SocketChat'
-}, {
-    'color': '#cba',
-    'user': 'User 2',
-    'msg': 'Thanks for invete me!'
-}, {
-    'color': '#bac',
-    'user': 'User 3',
-    'msg': 'Somone want to chat?'
-}]
+messeges = {
+    'lobby': [],
+    'members': []
+}
 
 # Flask or Socket.IO transport layer can be added here!
 
@@ -33,17 +24,27 @@ def client_connect():
 
     print('client connected')
 
+    room = 'lobby'
+
     users_online[request.sid] = {
         'username': f'Guest #{guest_vistor_count+1}',
         'rank': 'guest',
-        'connected_time': time.time()
+        'connected_time': time.time(),
+        'room': room
     }
     guest_vistor_count += 1
+    join_room(room)
 
     print(f'online users count: {len(users_online)}')
     print(users_online)
 
-    emit('MESSAGE', messeges)
+    messeges[room].append({
+        'color': 'yellow',
+        'user': 'System',
+        'msg': users_online[request.sid]['username'] +' has connected to this chat room'
+    })
+
+    emit('MESSAGE', messeges[room], broadcast=True, room=room)
     emit('USER_DATA', users_online[request.sid])
     emit('USER_ONLINE_PUBLIC_DATA', users_online)
 
@@ -51,7 +52,14 @@ def client_connect():
 @socketio.on('disconnect')
 def client_disconnect():
     print('client disconnect')
+    room = users_online[request.sid]['room']
+    messeges[room].append({
+        'color': 'pink',
+        'user': 'System',
+        'msg': users_online[request.sid]['username'] + ' has disconnected from the chat room'
+    })
 
+    emit('MESSAGE', messeges[room], broadcast=True, room=room)
     emit('USER_ONLINE_PUBLIC_DATA', users_online)
 
     del users_online[request.sid]
@@ -60,21 +68,54 @@ def client_disconnect():
 @socketio.on('SEND_MESSAGE')
 def client_send_message(data):
     if data['msg'].strip() != '':
+        room = users_online[request.sid]['room']
+
         print('client send message')
 
         if len(messeges) >= 5:
             print('pop the old messagt from the messagt queue')
             messeges.pop(0)
 
-        messeges.append({
+        messeges[room].append({
             'color': data['color'],
             'user': users_online[request.sid]['username'],
             'msg': data['msg']
         })
 
-        emit('MESSAGE', messeges, broadcast=True)
+        emit('MESSAGE', messeges[room], broadcast=True, room=room)
     else:
         print('empty message, its not sending back')
+
+
+@socketio.on('join')
+def client_join_room(data):
+    username = users_online[request.sid]['username']
+
+    leave_room = users_online[request.sid]['room']
+    go_to_room = data['room']
+
+    join_room(go_to_room)
+    users_online[request.sid]['room'] = go_to_room
+
+    messeges[leave_room].append({
+        'color': 'red',
+        'user': 'System',
+        'msg': username +' has left the room'
+    })
+
+    messeges[go_to_room].append({
+        'color': 'green',
+        'user': 'System',
+        'msg': username +' has join the room'
+    })
+
+    emit('MESSAGE', messeges[leave_room], broadcast=True, room=leave_room)
+    emit('MESSAGE', messeges[go_to_room], broadcast=True, room=go_to_room)
+
+
+@socketio.on('leave')
+def client_join_room(data):
+    pass
 
 
 if __name__ == '__main__':
